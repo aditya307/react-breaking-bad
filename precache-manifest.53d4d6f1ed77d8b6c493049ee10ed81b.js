5a911e9241550e39b4dacf90f9f14ed1ef2e65ca
AA <adi.2002.gwl@gmail.com>
2020-07-21 14:14:39 +0530

    Updates

---
+++ precache-manifest.53d4d6f1ed77d8b6c493049ee10ed81b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f5df66f4369d7c1f31c9ef79abe867",
+    "url": "/react-breaking-bad/index.html"
+  },
+  {
+    "revision": "0416f2e6ae261c0ded72",
+    "url": "/react-breaking-bad/static/css/main.746f10b6.chunk.css"
+  },
+  {
+    "revision": "665cebd579e754094a31",
+    "url": "/react-breaking-bad/static/js/2.03c3de83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-breaking-bad/static/js/2.03c3de83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0416f2e6ae261c0ded72",
+    "url": "/react-breaking-bad/static/js/main.78a2b350.chunk.js"
+  },
+  {
+    "revision": "2ea735ab257621a36f4e",
+    "url": "/react-breaking-bad/static/js/runtime-main.37c40f88.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/react-breaking-bad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/react-breaking-bad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/react-breaking-bad/static/media/spinner.235b9b3e.gif"
+  }
+]);
